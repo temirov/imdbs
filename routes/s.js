fded139f062a5b3034831efc082403cb2d3801ff
Vadym Tyemirov <temirov@gmail.com>
2013-07-26 12:20:25 -0400

    Charts for movei years and movies rating implemented

--- routes/s.js
+++ routes/s.js
@@ -14,11 +14,42 @@ redis_config.current = redis_config.c9.ip ? redis_config.c9 : redis_config.local
 var db = redis.createClient(redis_config.current.port, redis_config.current.ip);
 
 module.exports = function initS (app) {
-  app.get('/s', function(req, res) {
+  app.get('/s/years', function(req, res) {
     getYearsCount(function(err, years){
       res.json(years);
     });
   });
+
+  app.get('/s/ranks', function(req, res) {
+    getRanksCount(function(err, ranks){
+      res.json(ranks);
+    });
+  });
+
+};
+
+function getRanksCount(callback){
+  var ranks = [],
+    rank = 1.0,
+    err = null;
+
+  async.whilst(
+    function() { 
+      return rank <= 10; 
+    },
+    function(next) {
+      db.zcount("ranks", rank, rank, function(err, count){
+        // util.log(util.format("year is: %s, count is: %d", year, count));
+        ranks.push({r: rank, c: count});
+        rank = parseFloat((rank + 0.1).toFixed(1));
+        next();
+      });
+    },
+    function(err) {
+      callback(err, ranks);
+    }
+  );
+
 };
 
 function getYearsCount(callback){
